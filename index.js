7145d5c014484bb0fd419d619868388f076eefcd
Zach Fudge <zacheryfudge@gmail.com>
2018-03-24 08:56:41 -0600

    split to regex

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var server = http.createServer(function(req,res) {
 	res.writeHead(200, { 'Content-Type': 'application/json' });
 	var header = req.headers;
 	var obj = {
-		ipaddress: req.headers['x-forwarded-for'].split(',')[0] || req.connection.remoteAddress,
+		ipaddress: req.headers['x-forwarded-for'].replace(/\,.*$/, "")  || req.connection.remoteAddress,
 		language: header['accept-language'].split(",")[0],
 		software: header['user-agent'].split(/[()]/)[1]
 	};
